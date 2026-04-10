18bb5def626d6d9e9b5451f3bfa3a4b13c36f508
HARSH GARG <harsh.garg.1771@gmail.com>
2026-04-10 19:38:18 +0530

    DSA: solved CodeChef problem - sum comparison using basic math logic
    Solved a CodeChef DSA problem.
    
    Condition:
    Check if A + B > 2*C
    
    Time Complexity: O(1)
    Space Complexity: O(1)

---
+++ codechef/problem1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	int A,B,C;
+	cin>>A>>B>>C;
+
+	if(A+B>2*C){
+	    cout<<"YES\n";
+	}
+	else{
+	    cout<<"NO\n";
+	}
+}
+return 0;
+}
