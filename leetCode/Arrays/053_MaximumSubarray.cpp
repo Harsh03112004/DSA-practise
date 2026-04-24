4dbc657b867c51e9ee5ace93ae54d34153c91522
HARSH GARG <harsh.garg.1771@gmail.com>
2026-04-24 18:24:35 +0530

    LC #53 - Maximum Subarray - Kadane's Algorithm - O(n)

---
+++ leetCode/Arrays/053_MaximumSubarray.cpp
@@ -0,0 +1,13 @@
+// Maximum Subarray - Kadane's Algorithm - O(n)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentsum=nums[0];
+        int maxsum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            currentsum = max(nums[i],currentsum+nums[i]);
+            maxsum = max(maxsum,currentsum);
+        }
+        return maxsum;
+    }
+};
